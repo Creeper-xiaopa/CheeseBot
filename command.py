3fd459bbd97d3825e56ec33b4193ac7ef2b1c793
Creeper_xiaopa <i@xiaopa.cc>
2025-02-04 18:32:06 +0800

    add: status命令

--- command.py
+++ command.py
@@ -1,7 +1,10 @@
-from get_config import bot_config
 from log import log_msg
+from datetime import datetime 
 import send_request
 import yaml
+import platform
+import psutil
+import time
 
 def cmd_help(data, msg):
     send_request.send_group_msg(group_id=msg["group_id"], msg=f"[CQ:reply,id={msg["message_id"]}]{open('help.txt', 'r', encoding='utf-8').read()}")
@@ -9,6 +12,29 @@ def cmd_help(data, msg):
 def cmd_echo(data, msg):
     send_request.send_group_msg(group_id=msg["group_id"], msg=str(data))
 
+def cmd_status(data, msg):
+    cpu_usage = f"{psutil.cpu_percent(interval=1)}%"
+    mem = psutil.virtual_memory()
+    mem_usage = f"{mem.used/1024**3:.2f}GiB/{mem.total/1024**3:.2f}GiB"
+    system_v = f"{platform.system()} {platform.machine()} {platform.version()}"
+    current_time = datetime.now().strftime("%y/%m/%d %H:%M:%S")
+    net_io_start = psutil.net_io_counters()
+    time.sleep(1)
+    net_io_end = psutil.net_io_counters()
+    upload_speed = (net_io_end.bytes_sent - net_io_start.bytes_sent) * 8 / 1024 / 1024
+    download_speed = (net_io_end.bytes_recv - net_io_start.bytes_recv) * 8 / 1024 / 1024
+    upload = f"{upload_speed:.1f}Mbps"
+    download = f"{download_speed:.1f}Mbps"
+    message = f"""
+[CheeseBot 状态]
+版本: v0.1.0
+系统: {system_v}
+时间: {current_time}
+CPU: {cpu_usage}
+内存: {mem_usage}
+网络: {upload}↑ {download}↓
+""".strip()
+    send_request.send_group_msg(group_id=msg["group_id"], msg=f"[CQ:reply,id={msg["message_id"]}]{message}")
 
 def do(command, data, msg):
     # 从 YAML 文件中读取配置
